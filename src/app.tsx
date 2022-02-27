dc26d37d72eb69809f5e3c301254eaaac96783d8
sosukesuzuki <aosukeke@gmail.com>
2022-02-27 20:42:09 +0900

    Simply implementation

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,87 @@
+import { QueryClient, QueryClientProvider } from "react-query";
+import { useMemo, useState } from "preact/hooks";
+import { useThrottle } from "react-use";
+import { isNotFoundError, usePkgType } from "./use-pkg-type";
+
+function IsEsmPkgForm() {
+  const [packageName, setPackageName] = useState("");
+  const throttledPackageName = useThrottle(packageName);
+  const pkgType = usePkgType(throttledPackageName);
+  const isEsm = useMemo(() => pkgType.data === "module", [pkgType]);
+  const shouldShownAnswer = useMemo(() => {
+    if (throttledPackageName === "" || pkgType.isLoading || pkgType.isError) {
+      return false;
+    }
+    return true;
+  }, [throttledPackageName, pkgType]);
+  const shouldShownNotFound = useMemo(() => {
+    if (pkgType.isError && isNotFoundError(pkgType.error)) {
+      return true;
+    }
+    return false;
+  }, [pkgType]);
+  return (
+    <>
+      <form class="my-10">
+        <p class="text-3xl">
+          Is
+          <span class="px-3">
+            <input
+              type="text"
+              placeholder="package name"
+              class="border"
+              value={packageName}
+              onInput={(e) => {
+                setPackageName(
+                  // @ts-ignore
+                  e.target?.value ?? ""
+                );
+              }}
+            />
+          </span>
+          an ESM package?
+        </p>
+      </form>
+      <div class="text-3xl">
+        {shouldShownAnswer ? (
+          isEsm ? (
+            <p class="text-red-500">YES</p>
+          ) : (
+            <p class="text-blue-500">NO</p>
+          )
+        ) : null}
+        {shouldShownNotFound ? (
+          <p class="text-red-500">{throttledPackageName} not found.</p>
+        ) : null}
+      </div>
+    </>
+  );
+}
+
+const queryClient = new QueryClient();
 export function App() {
   return (
-    <div class="container mx-auto">
-      <h1 class="text-6xl">is-esm-pkg</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div class="container mx-auto">
+        <header class="my-10">
+          <h1 class="text-5xl font-bold">is-esm-pkg</h1>
+        </header>
+        <div class="my-10">
+          <p class="text-base">
+            is-esm-pkg is a website to check if a npm package is an{" "}
+            <a
+              href="https://gist.github.com/sindresorhus/a39789f98801d908bbc7ff3ecc99d99c"
+              target="_blank"
+              class="font-bold text-blue-500 hover:underline"
+            >
+              ESM package.
+            </a>
+          </p>
+        </div>
+        <main>
+          <IsEsmPkgForm />
+        </main>
+      </div>
+    </QueryClientProvider>
   );
 }
---
+++ src/use-pkg-type.ts
@@ -0,0 +1,46 @@
+import { useMemo } from "preact/hooks";
+import { useQuery } from "react-query";
+
+function getUnpkgUrl(packageName: string): string {
+  return `https://www.unpkg.com/${packageName}/package.json`;
+}
+
+type PkgType = "cjs" | "module" | undefined;
+function hasType(pkgJson: unknown): pkgJson is { type: PkgType } {
+  return (
+    Object.prototype.hasOwnProperty.call(pkgJson, "type") &&
+    // @ts-ignore
+    typeof pkgJson.type === "string"
+  );
+}
+function getTypeFromPkgJson(pkgJson: unknown): PkgType {
+  if (typeof pkgJson === "object" && pkgJson !== null && hasType(pkgJson)) {
+    return pkgJson.type;
+  }
+}
+
+class NotFoundError extends Error {
+  name = "NotFoundError";
+  constructor() {
+    super("Not Found");
+  }
+}
+export function isNotFoundError(error: unknown): error is NotFoundError {
+  return error instanceof NotFoundError;
+}
+export function usePkgType(packageName: string) {
+  const unpkgUrl = useMemo(() => getUnpkgUrl(packageName), [packageName]);
+  const pkgTypeResult = useQuery(packageName, async () => {
+    const res = await fetch(unpkgUrl);
+    if (!res.ok) {
+      if (res.status === 404) {
+        throw new NotFoundError();
+      }
+      throw res;
+    }
+    const responsJson = await res.json();
+    const pkgType = getTypeFromPkgJson(responsJson);
+    return pkgType;
+  });
+  return pkgTypeResult;
+}
